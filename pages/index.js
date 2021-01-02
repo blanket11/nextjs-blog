9308fbcd8efaa36f7b2ecb510c897a2f34d372e5
blanket11 <bkweb11k@gmail.com>
2021-01-02 13:07:48 +0900

    不要になったリンクを削除

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,6 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>『飛躍の飛躍の発展』 #RAYボリューション</p>
-        <p><Link href="posts/first-post"><a>詳細ページ</a></Link></p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
