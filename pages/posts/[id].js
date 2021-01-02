e5e4f1b08bc05f855d00d8e92c325fd6c7bbb7ec
blanket11 <bkweb11k@gmail.com>
2021-01-02 11:39:02 +0900

    markdownの本文を読み込み表示してみる

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,6 +9,9 @@ export default function Post({ postData }) {
             {postData.id}
             <br />
             {postData.date}
+            <br />
+            test: <br />
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>
     )
 }
@@ -24,7 +27,7 @@ export async function getStaticPaths() {
 
 // データを受け取る
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id)
+    const postData = await getPostData(params.id)
     return {
         props: {
             postData
